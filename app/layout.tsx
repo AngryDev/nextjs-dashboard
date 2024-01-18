4d384578f8bec9c2a9568366e969802d40952d12
Sergio Astorga <amelec2@gmail.com>
2024-01-18 18:21:37 -0500

    hasta el cap 4 del curso

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import React from 'react'
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>
+        <div className='relative h-16  bg-slate-400 '>
+          <h1 className='text-white font-bold absolute top-1/2 left-1/2 text-center transform -translate-x-1/2 -translate-y-1/2 '>
+            Hola Chiquitin!!
+          </h1>
+        </div>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
